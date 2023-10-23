795cabd95f94a08530cb1e0706528e996a71ec41
Praveen Perumal <ppraveen98841@gmail.com>
2023-10-23 10:46:58 +0530

    Added GET /attendance
    To fetch the imageUri given year,month,date and time
    TODO:  we need to incorporate image processing.
    
    Order time in response

--- main.py
+++ main.py
@@ -10,6 +10,33 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+def fetch_image_name(year: int, month: str, day: int, time: str):
+    """
+    year: int
+    month: str
+    day: int [1-31]
+    time: str (format: '%H:%M %p')
+    """
+
+    image_names = os.listdir('uploads')
+    month_names = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+    try:
+        hour, minutes = map(int, time.split(' ')[0].split(':'))
+        loc = time.split(' ')[-1]
+
+        if loc == 'AM':
+            hour = hour if hour < 12 else 0
+        else:
+            hour = hour + 12 if hour < 12 else 12
+        date = datetime.datetime(year, month_names.index(month) + 1, day, hour, minutes)
+        name = date.strftime("%Y-%m-%d-%H-%M")
+        image_name = list(filter(lambda n: n.split('.')[0] == name, image_names))[0]
+        return image_name
+    except:  # If something goes wrong return None
+        return None
+
+
 @app.get("/")
 async def root():
     image_names = os.listdir('uploads')
@@ -35,9 +62,18 @@ async def root():
             calendar[-1]["months"].append({"month": month, "dates": []})
 
             for date, times in date_dict.items():
-                calendar[-1]["months"][-1]["dates"].append({"date": date, "times": times})
+                calendar[-1]["months"][-1]["dates"].append({"date": date, "times": sorted(times, key=lambda time: time.split(' ')[::-1])})
 
     return {"message": "success", "calendar": calendar}
+
+
+@app.get("/attendance")
+def fetch_attendance(year: int, month: str, date: int, time: str):
+    image_name = fetch_image_name(year, month, date, time)
+    if image_name is None:
+        return {"message": "not found"}
+    else:
+        return {"message": "success", "imageUri": f"/uploads/{image_name}"}
             
 
 @app.post("/upload")
