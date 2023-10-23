2b5469701e93979c62986a65847590d8c047c49b
Praveen Perumal <ppraveen98841@gmail.com>
2023-10-23 10:18:46 +0530

    Added GET /
    Compute the calendar based on uploaded images

--- main.py
+++ main.py
@@ -8,9 +8,37 @@ import uvicorn
 from fastapi import FastAPI, UploadFile, Response
 from fastapi.staticfiles import StaticFiles
 
-
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    image_names = os.listdir('uploads')
+    year_dict = {}
+    calendar = []
+
+    for name in image_names:
+        date = datetime.datetime(*map(int, name.split('.')[0].split('-')))
+        year, month, day = date.year, date.strftime("%B"), date.day
+
+        if year not in year_dict:
+            year_dict[year] = {}
+        if month not in year_dict[year]:
+            year_dict[year][month] = {}
+        if day not in year_dict[year][month]:
+            year_dict[year][month][day] = []
+        year_dict[year][month][day].append(date.strftime("%I:%M %p"))
+
+    for year, month_dict in year_dict.items():
+        calendar.append({"year": year, "months": []})
+
+        for month, date_dict in month_dict.items():
+            calendar[-1]["months"].append({"month": month, "dates": []})
+
+            for date, times in date_dict.items():
+                calendar[-1]["months"][-1]["dates"].append({"date": date, "times": times})
+
+    return {"message": "success", "calendar": calendar}
+            
 
 @app.post("/upload")
 async def upload_file(file: UploadFile):
@@ -18,18 +46,18 @@ async def upload_file(file: UploadFile):
 
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
     file_extension = file.filename.split('.')[-1]
-    name = f"{timestamp}.{file_extension}"
-    path = os.path.join("uploads", name)
+    file_name = f"{timestamp}.{file_extension}"
+    path = os.path.join("uploads", file_name)
 
     async with aiofiles.open(path, "wb") as f:
         while chunk := await file.read(8192):
             await f.write(chunk)
 
-    print(path)
-    return {"message": "success", "path": f"/{path}"}
+    return {"message": "success", "imageUri": f"/{path}"}
 
 
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
